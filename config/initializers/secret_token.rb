0c040d7eb8f3a49ca30d8976c6d8a1ac5788ce96
Eric Iacutone <eric.iacutone@gmail.com>
2013-08-20 21:53:45 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodTruck::Application.config.secret_token = 'c1d7edd3bf41392097bed0b806050c7afc9c9da1858ba4e5d0734aad6f4111848173f281bc41a680fe7f6690938de9f8f210c38546c114696dcb3425c4a8996f'
