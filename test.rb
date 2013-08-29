acc126c19199d8ce3b8d91a1b8eb0a82bf570e4e
Eric Iacutone <eric.iacutone@gmail.com>
2013-08-29 14:29:22 -0400

    truck csv created

--- test.rb
+++ test.rb
@@ -1,36 +1,15 @@
-# require 'pry'
+require 'csv'
 
-# log = [
-#   {time: 201201, x: 2},
-#   {time: 201201, y: 7},
-#   {time: 201201, z: 2},
-#   {time: 201202, a: 3},
-#   {time: 201202, b: 4},
-#   {time: 201202, c: 0}
-# ]
-
-# @hash1 = {}
-# @hash2 = {}
-# log.each do |hash_element|
-# 	if hash_element[:time] == 201201
-# 		@hash1 = hash_element
-# 			@hash1.
-# 		end
-# 	else hash_element[:time] == 201202
-# 		@hash2 = hash_element
-# 	end
-# end
-
-	# def north_south_street(data)
-	# 	data.scan(/1st/).db_format && data.scan(/2nd/).db_format && data.scan(/3rd/).db_format && data.scan(/4th/).db_format && data.scan(/5th/).db_format && data.scan(/6th/).db_format && data.scan(/7th/).db_format && data.scan(/8th/).db_format && data.scan(/9th/i).db_format && data.scan(/10th/).db_format && data.scan(/11th/).db_format && data.scan(/12th/).db_format && data.scan(/Allen/i).db_format && data.scan(/Broad/i).db_format && data.scan(/Nassau/i).db_format && data.scan(/Lafayette/i).db_format && data.scan(/Broadway/i).db_format && data.scan(/Centre/i).db_format && data.scan(/Baxter/i).db_format && data.scan(/Church/i).db_format && data.scan(/Coenties/i).db_format && data.scan(/Chrystie/i).db_format && data.scan(/Elizabeth/i).db_format && data.scan(/Essex/i).db_format && data.scan(/Forsyth/i).db_format && data.scan(/Front/i).db_format && data.scan(/Greenwich/i).db_format && data.scan(/Hudson/i).db_format && data.scan(/Ludlow/i).db_format && data.scan(/Mott/i).db_format && data.scan(/Mulberry/i).db_format && data.scan(/Orchard/i).db_format && data.scan(/Whitehall/i).db_format && data.scan(/Pearl/i).db_format && data.scan(/South/i).db_format && data.scan(/Bowery/i).db_format && data.scan(/Spring/i).db_format && data.scan(/Sullivan/i).db_format && data.scan(/Thompson/i).db_format && data.scan(/Varick/i).db_format && data.scan(/William/i).db_format && data.scan(/West/i).db_format && data.scan(/York/i).db_format && data.scan(/First/i).db_format && data.scan(/Second/i).db_format && data.scan(/Third/i).db_format && data.scan(/Lexington/i).db_format && data.scan(/Fourth/i).db_format && data.scan(/Park/i).db_format && data.scan(/Vanderbilt/i).db_format && data.scan(/Madison/i).db_format && data.scan(/Fifth/i).db_format && data.scan(/Sixth/i).db_format && data.scan(/Seventh/i).db_format && data.scan(/Eighth/i).db_format && data.scan(/Ninth/i).db_format && data.scan(/Tenth/i).db_format && data.scan(/Eleventh/i).db_format && data.scan(/Twelfth/i).db_format
-	# end
-
-	# def east_west_street(data)
-	# 	data.scan(/1st/).db_format && data.scan(/2nd/).db_format && data.scan(/3rd/).db_format && data.scan(/4th/).db_format && data.scan(/5th/).db_format && data.scan(/6th/).db_format && data.scan(/7th/).db_format && data.scan(/8th/).db_format && data.scan(/9th/i).db_format && data.scan(/10th/).db_format && data.scan(/11th/).db_format && data.scan(/12th/).db_format && data.scan(/13th/).db_format && data.scan(/14th/).db_format && data.scan(/15th/).db_format && data.scan(/10th/).db_format && data.scan(/10th/).db_format && data.scan(/10th/).db_format && data.scan(/16th/).db_format && data.scan(/17th/).db_format && data.scan(/18th/).db_format && data.scan(/19th/).db_format && data.scan(/20th/).db_format && data.scan(/21st/).db_format && data.scan(/22nd/).db_format && data.scan(/23rd/).db_format && data.scan(/24th/).db_format && data.scan(/25th/).db_format && data.scan(/26th/).db_format && data.scan(/27th/).db_format && data.scan(/28th/).db_format && data.scan(/29th/).db_format && data.scan(/30th/).db_format && data.scan(/31st/).db_format && data.scan(/32nd/).db_format && data.scan(/33rd/).db_format && data.scan(/34th/).db_format && data.scan(/35th/).db_format && data.scan(/36th/).db_format && data.scan(/37th/).db_format && data.scan(/38th/).db_format && data.scan(/39th/).db_format && data.scan(/40th/).db_format && data.scan(/41st/).db_format && data.scan(/42nd/).db_format && data.scan(/43rd/).db_format && data.scan(/44th/).db_format && data.scan(/45th/).db_format && data.scan(/46th/).db_format && data.scan(/47th/).db_format && data.scan(/48th/).db_format && data.scan(/49th/).db_format && data.scan(/50th/).db_format && data.scan(/51st/).db_format && data.scan(/52nd/).db_format && data.scan(/53rd/).db_format && data.scan(/54th/).db_format && data.scan(/55th/).db_format && data.scan(/56th/).db_format && data.scan(/57th/).db_format && data.scan(/58th/).db_format && data.scan(/59th/).db_format 
-	# end 
-
-		# require 'Indirizzo'
-		# require 'pry'
-		# street = "52 hello stuuf"
-		# Indirizzo::Address.new(street)
-
+task :trucks => :environment do
+	CSV_HEADERS = ['id','name', 'twitter']
+	CSV.open('trucks.csv','w') do |csv|
+	 csv << CSV_HEADERS
+	 Truck.find_each do |t|
+	   csv << [
+	     t.id,
+	     t.name,
+	     t.twitter
+	   ]
+	 end
+	end
+end
