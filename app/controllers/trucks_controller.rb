6503b7cb52f59b802a96e793fb10e79b13da11c2
AlexGorski <alex.gorski@gmail.com>
2014-10-24 23:21:41 -0400

    Add actions for Trucks controller

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -1,2 +1,47 @@
 class TrucksController < ApplicationController
+  # GET /trucks
+  # GET /trucks.json
+  def index
+    @trucks = Truck.all
+
+    render json: @trucks
+  end
+
+  # GET /trucks/1
+  # GET /trucks/1.json
+  def show
+    @truck = Truck.find(params[:id])
+
+    render json: @truck
+  end
+
+  # POST /trucks
+  def create
+    @truck = Truck.new(params[:truck])
+
+    if @truck.save
+      render json: @truck, status: :created, location: @truck
+    else
+      render json: @truck.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PUT /trucks/1
+  def update
+    @truck = Truck.find(params[:id])
+
+    if @truck.update_attributes(params[:truck])
+      head :no_content
+    else
+      render json: @truck.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /trucks/1
+  def destroy
+    @truck = Truck.find(params[:id])
+    @truck.destroy
+
+    head :no_content
+  end
 end
